edb9e17e55b29b99d1e9594635fe5a6f052300a4
moseLange <104142100+moseLange@users.noreply.github.com>
2022-05-26 22:25:23 +0200

    Add files via upload

---
+++ JavaApplication_A3/build/classes/javaapplication_a3/JavaApplication_A3.rs
@@ -0,0 +1,7 @@
+LabApp
+Computer
+LabApp$1
+LabApp$3
+LabApp$2
+LabApp$5
+LabApp$4
---
+++ JavaApplication_A3/build/classes/javaapplication_a3/LabApp.rs
@@ -0,0 +1,7 @@
+javaapplication_a3.LabApp$5
+javaapplication_a3.Computer
+javaapplication_a3.LabApp$2
+javaapplication_a3.LabApp
+javaapplication_a3.LabApp$1
+javaapplication_a3.LabApp$4
+javaapplication_a3.LabApp$3
